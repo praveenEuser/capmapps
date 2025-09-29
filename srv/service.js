638e3e3cfe3e497cfec5b4a9a3860aebf13cb3f2
praveenEuser <praveenkumarofficialacc@gmail.com>
2025-09-29 17:55:25 +0000

    actions

--- srv/service.js
+++ srv/service.js
@@ -1,8 +1,25 @@
 module.exports = cds.service.impl(async function() {
+
+    const {PurchaseSet} = this.entities;
+
     this.on('boost',async(req)=> {
         console.log("Done bro");
-        return {
-            "NODE_KEY" : "Dummy"
-        };
+        
+        try {
+
+            const ID = req.params[0];
+            // Start a DB Transaction
+            const tx = cds.tx(req);
+
+            //CDS Query Language : Communicate to DB in agnostic manner
+            await tx.update(PurchaseSet).with({
+                GROSS_AMOUNT : {"+=" : 2000}
+            }).where(ID);
+        } catch (error) {
+            return "Error" + error.toString();
+        }
+
+
+
     });
 });
